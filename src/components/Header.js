4e4cb1a647b505f38105de1d3e5ce0f4a46d20fa
beem10101 <beem25301@gmail.com>
2022-07-17 10:40:55 +0700

    10:40 17/7/22

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,27 +2,27 @@ import React from 'react'
 import Typed from 'react-typed'
 
 import logo from '../logo.png'
+import contract1 from '../contract_1.PNG'
+import etherscan1 from '../etherscan_1.PNG'
 
 function Header() {
   return (
-    // <div className='header-wrapper'>
-    //     <div className='main-info'>
-    //     <h1>port1</h1>
-    //     <Typed className='typed-text'
-    //         strings={['Welcome','to','myportfolio!']}
-    //         typeSpeed={120}
-    //         backSpeed={60}
-    //         loop
-    //     />
-    //     </div>
-    // </div>
-
     <div>
+    <div className='header-wrapper'>
+        <div className='main-info'>
+        <h1>port1</h1>
+        <Typed className='typed-text'
+            strings={['Welcome','to','myportfolio!']}
+            typeSpeed={120}
+            backSpeed={60}
+            loop
+        />
+        <div>
     <div className='container'>
         <br></br>
         <br></br>
         <br></br>
-        <img class="img-circle loaded" src={logo} width="180" height="180" data-was-processed="true" ></img>
+        <img className="rounded-circle" src={logo} width="180" height="180" data-was-processed="true" ></img>
         <br></br>
         <br></br>
     </div>
@@ -30,28 +30,46 @@ function Header() {
     <a className="anchor" id="about"></a>
     <div className="followWrap">
         <div className="followMeBar fixed">
-            <h2>About me</h2>
+            <h1><b>About me</b></h1>
         </div>
     </div>
-    <p >Email: kriskanin.h@gmail.com</p>
-    <p>GitHub : <a href="https://github.com/KritzHeng" target="_blank">KritzHeng</a></p>   
-    LinkedIn : <a href="https://www.linkedin.com/in/kriskanin/" target="_blank">Kriskanin Hengniran</a>
+    <p >Email: beem25301@gmail.co</p>
+    <p>GitHub : <a href="https://github.com/beem10101" target="_blank">beem10101</a></p>   
+    {/* <p>LinkedIn : <a href="https://www.linkedin.com/in/kriskanin/" target="_blank">Kriskanin Hengniran</a></p> */}
       <div className="followMeBar fixed">
 
-      <h2>Education</h2>
+      <h1><b>Education</b></h1>
   <div>
       <ul >
-          <p>2019 - present</p>
-          <strong>B.S. Computer Science</strong>
-          <strong>Kasetsart University</strong> (Bangkok, Thailand)
-      </ul>
-      <ul >
-          <p>2016 - 2018</p>
-          <strong>Triamudom Suksa Pattanakarn Ratchada School</strong> (Bangkok, Thailand)
+        <p >2017 - present :
+            {/* <strong>high school</strong> */}
+            <strong className='textFont'> thammasat klongluang wittayakom</strong> (pathumthani, Thailand)
+        </p>
       </ul>
     </div>
   </div>
   </div>
+        </div>
+        </div>
+    </div>
+
+  <div className='HeaderTask'>
+    <div>
+      <p>project1</p>
+      <p>aaa</p>
+      <img src={contract1}></img>
+    </div>
+    <div>
+      <p>project name</p>
+      <p>ทำอะไรไปบ้าง....</p>
+      <img src={etherscan1}></img>
+    </div>
+    <div>
+      <p>project3</p>
+      <p>aaa</p>
+    </div>
+  </div>
+
   </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <div className="container">
                 
-            <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."></img></a>
+            {/* <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."></img></a> */}
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 {/* <span className="navbar-toggler-icon"></span> */}
                 <FontAwesomeIcon icon={faBars} style={{color: "#ffff"}} />
