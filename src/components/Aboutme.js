e928a1d03666ee54e4ce0de35d88bb13321eb55f
beem10101 <beem25301@gmail.com>
2022-08-20 12:24:49 +0700

    aboutme rechart

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,12 +1,93 @@
 // import React from 'react'
 import profile1 from '../steve1.png'
+import Footer from './Footer';
 
 import React, { PureComponent } from 'react';
 import { ResponsiveContainer, PieChart, Pie, Legend, Cell } from 'recharts';
-
-
-
-
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis} from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+const dataBar = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 1000,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+const dataRadar = [
+  {
+    subject: 'Math',
+    A: 120,
+    B: 110,
+    fullMark: 150,
+  },
+  {
+    subject: 'Chinese',
+    A: 98,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'English',
+    A: 86,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'Geography',
+    A: 99,
+    B: 100,
+    fullMark: 150,
+  },
+  {
+    subject: 'Physics',
+    A: 85,
+    B: 90,
+    fullMark: 150,
+  },
+  {
+    subject: 'History',
+    A: 65,
+    B: 85,
+    fullMark: 150,
+  },
+];
 const data = [
     { name: 'Group A', value: 100, detail: "Python" },
     { name: 'Group B', value: 300, detail: "Java" },
@@ -26,10 +107,15 @@ const Aboutme = () => {
     <div>
       <div className='header-wrapper'>
         <div className='main-info'>
+          <br/>
+          <br/>
+          <br/>
           <div>
-            {/* Abuot me */}
-            <h1><b>About me</b></h1>
-            <div className='banner-about'>
+          {/* grid about me */}
+          <div class="row">
+            <div class="col-sm">
+              {/* Abuot me */}
+              <div className='banner-about'>
               <div className='container'>
                 <br />
                 <br />
@@ -48,23 +134,31 @@ const Aboutme = () => {
                   {/* <p>LinkedIn : <a href="https://www.linkedin.com/in/kriskanin/" target="_blank">Kriskanin Hengniran</a></p> */}
                 </div>
               </div>
+              </div>
             </div>
-            <br />
-            <br />
-            <br />
-            {/* Education */}
-            <div className='banner-about'>
+            <div class="col-sm">
+              {/* Education */}
+              <div className='banner-about'>
               <div className="followMeBar fixed">
                 <h1><b>Education</b></h1>
                 <div>
                   <ul >
                     <p >2017 - present :
-                      <strong className='textFont'> thammasat klongluang wittayakom</strong> (pathumthani, Thailand)
+                      <strong className='textFont'> thammasat klongluang withayakhom</strong> (pathumthani, Thailand)
+                      <p>----</p>
+                      <p>----</p>
+                      <p>----</p>
+                      <p>----</p>
+                      <p>----</p>
+                      <p>----</p>
+                      <p>----</p>
                     </p>
                   </ul>
                 </div>
               </div>
+              </div>
             </div>
+          </div>
             <br />
             <br />
             <br />
@@ -96,8 +190,53 @@ const Aboutme = () => {
                 </div>
               </div>
             </div>
+            <br />
+            <br />
+            <br />
+            {/* radar */}
+            <div className='banner-about'>
+              <ResponsiveContainer width={700} height={500}>
+                <RadarChart cx="50%" cy="50%" outerRadius="80%" data={dataRadar}>
+                  <PolarGrid />
+                  <PolarAngleAxis dataKey="subject" />
+                  <PolarRadiusAxis />
+                  <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                </RadarChart>
+              </ResponsiveContainer>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            {/* bar */}
+            <div className='banner-about'>
+              <ResponsiveContainer width={700} height={500}>
+                <BarChart
+                  width={500}
+                  height={300}
+                  data={dataBar}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="pv" name='data1' fill="#000000" />
+                  <Bar dataKey="pv" fill="#011235" />
+                  <Bar dataKey="pv" fill="#035487" />
+                  <Bar dataKey="pv" fill="#078452" />
+                  {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
           </div>
         </div>
+        <Footer/>
       </div>
     </div>
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <section>
+            <p className='footer-subscription-heading'>
+                123456789
+            </p>
+        </section>
+    </div>
+  )
+}
+
+export default Footer
